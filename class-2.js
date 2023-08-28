c1bfa29b99a604f0556f7481102fa86ff6ed7c65
Ayeshafatimach2 <ayeshafatimach5@gmail.com>
2023-08-28 08:21:08 -0700

    Assignment

---
+++ class-2.js
@@ -0,0 +1,72 @@
+"use strict";
+//Addition
+var num1 = 10;
+var num2 = 20;
+var result = num1 + num2;
+console.log(result);
+// Substraction
+var nu1 = 10;
+var nu2 = 20;
+var result = nu1 - nu2;
+console.log(result);
+// Multiplication
+var nb1 = 10;
+var nb2 = 20;
+var result = nb1 * nb2;
+console.log(result);
+// Division
+var _n1 = 10;
+var _n2 = 20;
+var result = _n1 / _n2;
+console.log(result);
+// Exponentions
+var $1 = 10;
+var $2 = 20;
+var result = $1 ** $2;
+console.log(result);
+// Modulus
+var myPercentage1 = 10;
+var myPercentage2 = 3;
+var result = myPercentage1 % myPercentage2;
+console.log(result);
+// Assignment operator
+let n$ = 10;
+n$ += 5;
+console.log(n$);
+// Comparison Operator
+var $n1 = 15;
+var $n2 = 10;
+var output = $n1 < $n2;
+console.log(output);
+// Logical Operator
+// Example-1
+var _$1 = 9;
+var _$2 = 5;
+var output = 5 == 5 && _$1 == _$2;
+console.log(output);
+// Example-2
+var nb1 = 17;
+var nb2 = 15;
+var ans = 5 == 5 && nb1 == nb2;
+console.log(ans);
+// Example-3
+var nr1 = 17;
+var nr2 = 15;
+var ans = nr2 == nr2 || nr1 == nr2;
+console.log(ans);
+// Example-4
+var userName = "Ayesha";
+var age = 20;
+var nationality = "Pakistani";
+var answer = age > 18 && nationality == "Pakistani";
+console.log(answer);
+// All Arithmatic Operator
+var result2 = num2 + nu1 - nb2 * _n1 / $2 ** myPercentage1 % nr2;
+console.log(result2);
+// Type of variable
+var userNum = 10;
+console.log(typeof userNum);
+// Template Literals
+var myName = "Ayesha";
+var myName2 = `Fatima ${myName}`;
+console.log(myName2);
---
+++ class-2.ts
@@ -0,0 +1,101 @@
+//Addition
+var num1:number=10;
+var num2:number=20;
+var result:number=num1+num2;
+console.log(result);
+
+// Substraction
+var nu1:number=10;
+var nu2:number=20;
+var result:number=nu1-nu2;
+console.log(result);
+
+// Multiplication
+var nb1:number=10;
+var nb2:number=20;
+var result:number=nb1*nb2;
+console.log(result);
+
+// Division
+var _n1:number=10;
+var _n2:number=20;
+var result:number=_n1/_n2;
+console.log(result);
+
+// Exponentions
+var $1:number=10;
+var $2:number=20;
+var result:number=$1**$2;
+console.log(result);
+
+// Modulus
+var myPercentage1:number=10;
+var myPercentage2:number=3;
+var result:number=myPercentage1%myPercentage2;
+console.log(result);
+
+// Assignment operator
+let n$:number=10;
+n$ += 5;
+console.log(n$);
+
+// Comparison Operator
+var $n1:number=15;
+var $n2:number=10;
+var output:boolean=$n1 < $n2;
+console.log(output);
+
+// Logical Operator
+
+// Example-1
+var _$1:number=9;
+var _$2:number=5;
+var output:boolean= 5==5 && _$1==_$2;
+console.log(output);
+// Example-2
+var nb1:number = 17;
+var nb2:number = 15;
+var ans:boolean = 5==5 && nb1==nb2;
+console.log(ans);
+// Example-3
+var nr1:number = 17;
+var nr2:number = 15;
+var ans:boolean = nr2==nr2 || nr1==nr2;
+console.log(ans);
+// Example-4
+var userName:string = "Ayesha";
+var age: number = 20;
+var nationality:string= "Pakistani";
+var answer : boolean=age>18 && nationality=="Pakistani";
+console.log(answer);
+
+// All Arithmatic Operator
+var result2= num2+nu1-nb2*_n1/$2**myPercentage1%nr2;
+console.log(result2);
+
+// Type of variable
+var userNum:number=10;
+console.log(typeof userNum);
+
+// Template Literals
+var myName:string="Ayesha";
+var myName2:string=`Fatima ${myName}`;
+console.log(myName2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
